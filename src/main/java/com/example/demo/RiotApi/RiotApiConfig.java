16272517422683fbbe6d9003335a13a939899702
OssianM5 <130681352+OssianM5@users.noreply.github.com>
2025-01-13 08:46:11 +0200

    commitLast

--- src/main/java/com/example/demo/RiotApi/RiotApiConfig.java
+++ src/main/java/com/example/demo/RiotApi/RiotApiConfig.java
@@ -1,7 +1,7 @@
 package com.example.demo.RiotApi;
 
 public class RiotApiConfig {
-    private static final String API_KEY = "RGAPI-cc39bf8c-cdba-4009-a721-e4ca16636592"; // Replace with your actual key
+    private static final String API_KEY = "RGAPI-6e55a5f7-ed96-4cca-a8fd-97209cf50497"; // Replace with your actual key
 
     public static String getApiKey() {
         return API_KEY;
